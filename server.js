b05012d8c7087b1dbc578fa0444a9525a4ce3b77
alexthedar <alex.castaneda@gmail.com>
2017-02-13 16:14:15 -0800

    test w/ server.js file

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+// 
+//
+// var express = require('express')
+// var path = require('path')
+// var serveStatic = require('serve-static')
+// var touch = require('touch')
+// var app = express()
+// var listener = process.env.PORT || 8000
+//
+// app.use(serveStatic(path.join(__dirname, 'dist')))
+//
+// app.get('*',function (req, res) {
+//     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
+//
+//
+// app.listen(listener)
+//
+// console.log('server started ' + listener)
+// touch('/tmp/app-initialized')
+// console.log('nginx notified to start traffic.')
 
-
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-var touch = require('touch')
-var app = express()
-var listener = process.env.NODE_PORT || 8000
-
-app.use(serveStatic(path.join(__dirname, 'dist')))
-
-app.get('*',function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-
-app.listen(listener)
-
-console.log('server started ' + listener)
-touch('/tmp/app-initialized')
-console.log('nginx notified to start traffic.')
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
